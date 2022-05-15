dfd3cfbe8a5324f170e94b96f6c8a394c07042cb
FZZStar <2631674847@qq.com>
2022-05-15 13:21:08 +0800

    IDEA first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author fzzstart
+ * @create 2022-05-15 13:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
