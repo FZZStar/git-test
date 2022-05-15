0856c16c8730c716f2df6ed828e4a83ee5e1d940
FZZStar <2631674847@qq.com>
2022-05-15 14:27:19 +0800

    IDEA master test push1 commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello hot-fix 4");
         System.out.println("hello hot-fix 5");
         System.out.println("hello master 6");
+        System.out.println("master push 1");
     }
 }
