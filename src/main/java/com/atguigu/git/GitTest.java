44b6610dc3458aed62d1b8a8b19c54ac5e1d617d
FZZStar <2631674847@qq.com>
2022-05-15 14:02:22 +0800

    IDEA hot-fix second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello git 2");
         System.out.println("hello git 3");
         System.out.println("hello hot-fix 4");
+        System.out.println("hello hot-fix 5");
     }
 }
